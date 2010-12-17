4bbd9cb4993baf724367e5b2876c2701f48537c1
iromli <izman.romli@gmail.com>
2010-12-17 10:57:03 +0700

    yes, another re-organizing work

--- scripts/01-base.sh
+++ scripts/01-base.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 echo "copying gnome2 to ~/.gnome2/"
-cp -R ../gnome2/* ~/.gnome2/
+cp -R ../build/gnome2/* ~/.gnome2/
 
 echo "copying local to ~/.local/"
-cp -R ../local/* ~/.local/
+cp -R ../build/local/* ~/.local/
 
 echo "copying nanorc to ~/.nanorc"
-cp ../nanorc ~/.nanorc
+cp ../src/nanorc ~/.nanorc
 
 echo "copying bash_aliases to ~/.bash_aliases"
-cp ../bash_aliases ~/.bash_aliases
+cp ../src/bash_aliases ~/.bash_aliases
 
 echo "copying bashrc to ~/.bashrc"
-cp ../bashrc ~/.bashrc
+cp ../src/bashrc ~/.bashrc
 
