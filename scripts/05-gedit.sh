ec58772ef3cc1a862090db027c34307bacb37f48
iromli <izman.romli@gmail.com>
2010-12-20 10:20:35 +0700

    some gedit goodies were removed and some were added

--- scripts/05-gedit.sh
+++ scripts/05-gedit.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-if [ -z `which git` ]; then
-    echo "please install git first"
-    exit
+if [ ! -z `which git` ]; then
+    git submodule update
 fi
-
-git submodule update
 
 # SCRIPT_DIR
 cd `dirname $0`
 
-PLUGINS=../build/gnome2/gedit/plugins
+PLUGINS=~/.gnome2/gedit/plugins
 SRC=../src
 GMATE_PLUGINS=$SRC/gmate/plugins
 
@@ -18,9 +15,7 @@ if [ ! -d $PLUGINS ]; then
     mkdir -p $PLUGINS
 fi
 
-#========================================================================
-# PLUGINS
-#========================================================================
+#=== PLUGINS ===#
 
 # align
 echo "=> align"
@@ -33,6 +28,10 @@ cp -R $SRC/gedit-plugin-autocomplete/* $PLUGINS
 # classbrowser
 echo "=> classbrowser"
 cp -R $SRC/gedit-plugin-classbrowser/* $PLUGINS
+
+# clientside
+echo "=> clientside"
+cp -R $SRC/gedit-plugin-clientside/* $PLUGINS
 
 # folding
 echo "=> folding"
@@ -63,6 +62,10 @@ cp -R $GMATE_PLUGINS/smart_indent* $PLUGINS
 echo "=> snapopen"
 cp -R $SRC/gedit-plugin-snapopen/* $PLUGINS
 
+# tabs_extend
+echo "=> tabs_extend"
+cp -R $SRC/gedit-plugin-tabs_extend/* $PLUGINS
+
 # tabswitch
 echo "=> tabswitch"
 cp -R $GMATE_PLUGINS/tabswitch* $PLUGINS
@@ -79,18 +82,24 @@ cp -R $SRC/gedit-plugin-zencoding/* $PLUGINS
 rm $PLUGINS/*.rst
 rm $PLUGINS/*.md
 rm -rf $PLUGINS/*.git*
-
-# lets copy those files to ~/.gnome2/gedit/plugins
-GEDIT_PLUGINS=~/.gnome2/gedit/plugins
-if [ ! -d $GEDIT_PLUGINS ]; then
-    mkdir $GEDIT_PLUGINS
-fi
-
 echo "installed to ~/.gnome2/gedit/plugins"
-cp -R $PLUGINS/* $GEDIT_PLUGINS
-rm -rf ../build/gnome2
 
 # codecomment, colorpicker, sessionsaver, smartspaces, terminal installed through synaptic
 echo "grab other plugins from package manager"
 yes | sudo aptitude install gedit-plugins
+echo "installed to /usr/lib/gedit-2/plugins"
+
+#=== LANGUAGE-SPECS ===#
+
+SPECS=~/.local/share/gtksourceview-2.0/language-specs
+
+if [ ! -d $SPECS ]; then
+    mkdir -p $SPECS
+fi
+
+# coffeescript
+echo "=> coffeescript"
+cp -R $SRC/gedit-lang-coffeescript/coffee_script.lang $SPECS/coffee_script.lang
+
+echo "installed to ~/.local/share/gtksourceview-2.0/language-specs"
 
