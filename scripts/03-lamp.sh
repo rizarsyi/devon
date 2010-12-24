3778816052c2e6223682a92c069c47fdeb2fb4b3
iromli <izman.romli@gmail.com>
2010-12-24 12:33:08 +0700

    added libmysqlclient*-dev to satisfy MySQL-python

--- scripts/03-lamp.sh
+++ scripts/03-lamp.sh
@@ -4,7 +4,7 @@
 yes | sudo aptitude install apache2 apache2-mpm-prefork apache2-prefork-dev apache2-utils apache2.2-common
 
 # install MySQL
-yes | sudo aptitude install mysql-server-5.1 mysql-client-5.1
+yes | sudo aptitude install mysql-server-5.1 mysql-client-5.1 libmysqlclient16-dev
 
 # install PHP
 yes | sudo aptitude install php5 php5-xdebug php5-suhosin php5-mysql php5-gd libapache2-mod-php5
