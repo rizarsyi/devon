f830041091574ba8df9d7f4feca5ca7e7d110b31
Isman Firmansyah <izman.romli@gmail.com>
2011-02-27 16:09:04 +0700

    cleanup

--- devon-gedit.sh
+++ devon-gedit.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-if [ ! -z `which git` ]; then
-    git submodule update
-fi
-
 # SCRIPT_DIR
 cd `dirname $0`
-SRC=../src
+SRC=./gedit
 
 #=== PLUGINS ===#
 PLUGINS=~/.gnome2/gedit/plugins
 if [ ! -d $PLUGINS ]; then
     mkdir -p $PLUGINS
 fi
-cp -R $SRC/gedit/plugins/* $PLUGINS/
+cp -R $SRC/plugins/* $PLUGINS/
 echo "grabbing other plugins from package manager"
 yes | sudo aptitude install gedit-plugins
 
@@ -22,21 +18,21 @@ SPECS=~/.local/share/gtksourceview-2.0/language-specs
 if [ ! -d $SPECS ]; then
     mkdir -p $SPECS
 fi
-cp -R $SRC/gedit/lang-specs/* $SPECS/
+cp -R $SRC/lang-specs/* $SPECS/
 
 #=== SNIPPETS ===#
 SNIPPETS=~/.gnome2/gedit/snippets
 if [ ! -d $SNIPPETS ]; then
     mkdir -p $SNIPPETS
 fi
-cp -R $SRC/gedit/snippets/* $SNIPPETS/
+cp -R $SRC/snippets/* $SNIPPETS/
 
 #=== STYLES ===#
 STYLES=~/.gnome2/gedit/styles
 if [ ! -d $STYLES ]; then
     mkdir -p $STYLES
 fi
-cp -R $SRC/gedit/styles/* $STYLES/
+cp -R $SRC/styles/* $STYLES/
 
 echo "Plugins  => ~/.gnome2/gedit/plugins, /usr/lib/gedit-2/plugins"
 echo "Language => ~/.local/share/gtksourceview-2.0/language-specs"
--- scripts/04-python.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# GIT AWESOMENESS
-yes | sudo aptitude install python-setuptools python-twisted libapache2-mod-wsgi
-sudo easy_install -U pip
-sudo pip install virtualenv nose bpython should-dsl
-
